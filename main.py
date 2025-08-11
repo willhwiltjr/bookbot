71e999d80e1bb864f5e00f6dd98704ce7fa14499
willhwiltjr <wiltwijr@gmail.com>
2025-08-11 13:11:02 -0700

    finished refactor lesson

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
+from stats import num_words
+
 def get_book_text(f):
     with open(f) as g:
         temp=g.read()
         return temp
 
-def num_words(string):
-    temp=string.split()
-    num=len(temp)
-    return num
+
 
 def main():
     print(f"{num_words(get_book_text("books/frankenstein.txt"))} words found in the document")
---
+++ stats.py
@@ -0,0 +1,4 @@
+def num_words(string):
+    temp=string.split()
+    num=len(temp)
+    return num
