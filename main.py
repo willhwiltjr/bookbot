8c68047ecb0d2df0c9d6b8890226d3b33bf6f7ec
willhwiltjr <wiltwijr@gmail.com>
2025-08-11 16:15:37 -0700

    ch3reportL1 solution

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
-from stats import num_words
+from stats import num_words, char_count, pairs
 
 def get_book_text(f):
     with open(f) as g:
         temp=g.read()
         return temp
 
+def artwork(url):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {url}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words(get_book_text(url))} total words")
+    print("--------- Character Count -------")
+    temp =pairs(char_count(get_book_text(url)))
+    for element in temp:
+        if element["char"].isalpha():
+            print(f"{element["char"]}: {element["num"]}")
+    print("============= END ===============")
+
+
 
 
 def main():
-    print(f"{num_words(get_book_text("books/frankenstein.txt"))} words found in the document")
+    artwork("books/frankenstein.txt")
 
 main()
 
--- stats.py
+++ stats.py
@@ -2,3 +2,32 @@ def num_words(string):
     temp=string.split()
     num=len(temp)
     return num
+
+def char_count(input):
+    chars={}
+    for char in input:
+        lower=char.lower()
+        if "a" <= lower <= "z":
+            if lower in chars:
+                chars[lower]+=1
+            else:
+                chars[lower]=1
+    return chars
+
+def sort_on(items):
+    return items["num"]
+
+def pairs(dict):
+    result=[]
+    char=""
+    num=0
+    for key, value in dict.items():
+        temp={"char": "","num": 0}
+        char = key
+        num = value
+        temp["char"]=char
+        temp["num"]=num
+        result.append(temp)
+    result.sort(reverse=True, key=sort_on)
+    return result
+        
